345a4c5588938b7a66db46341dae5e21adf3a8e6
Tiffany <67019104+tiffany-m@users.noreply.github.co>
2024-01-25 14:58:07 -0500

    Fixed styling added required error message

--- Form Practice/scripts.js
+++ Form Practice/scripts.js
@@ -1,94 +1,106 @@
 // Constraint API
 // NOTES:
-document.addEventListener("DOMContentLoaded", (event) => {
-  const sampleForm = document.getElementById("sample-form");
-  const emailInput = document.getElementById("email-input");
-  const countryInput = document.getElementById("country-input");
-  const zipcodeInput = document.getElementById("zipcode-input");
-  const passwordInput = document.getElementById("password-input");
-  const confirmPasswordInput = document.getElementById(
-    "confirm-password-input"
-  );
-  const inputs = [
-    emailInput,
-    countryInput,
-    zipcodeInput,
-    passwordInput,
-    confirmPasswordInput,
-  ];
 
-  sampleForm.addEventListener("submit", function (e) {
-    inputs.forEach((input) => {
-      console.log(input);
-      if (!input.validity.valid) {
-        e.preventDefault();
-        input.classList.add("error");
-      } else {
-        input.classList.remove("error");
-      }
-    });
-  });
+const sampleForm = document.getElementById("sample-form");
+const emailInput = document.getElementById("email-input");
+const countryInput = document.getElementById("country-input");
+const zipcodeInput = document.getElementById("zipcode-input");
+const passwordInput = document.getElementById("password-input");
+const confirmPasswordInput = document.getElementById("confirm-password-input");
+const inputs = [
+  emailInput,
+  countryInput,
+  zipcodeInput,
+  passwordInput,
+  confirmPasswordInput,
+];
 
-  emailInput.addEventListener("input", (event) => {
-    if (emailInput.validity.typeMismatch) {
-      emailInput.setCustomValidity("Please enter a valid email address.");
-      emailInput.reportValidity();
+sampleForm.addEventListener("submit", function (e) {
+  inputs.forEach((input) => {
+    if (!input.validity.valid) {
+      e.preventDefault();
+      input.classList.add("error");
     } else {
-      emailInput.setCustomValidity("");
+      input.classList.remove("error");
     }
   });
 
-  countryInput.addEventListener("input", function () {
-    if (countryInput.validity.patternMismatch) {
-      countryInput.setCustomValidity("Please eneter a valid country name.");
-      countryInput.reportValidity();
-    } else {
-      countryInput.setCustomValidity("");
-    }
-  });
-
-  zipcodeInput.addEventListener("input", function () {
-    if (zipcodeInput.validity.patternMismatch) {
-      zipcodeInput.setCustomValidity("Zipcode must be 5 digits long");
-      zipcodeInput.reportValidity();
-    } else {
-      zipcodeInput.setCustomValidity("");
-    }
-  });
-
-  passwordInput.addEventListener("input", function () {
-    let password = passwordInput.value;
-    let validityMessage = "";
-
-    if (passwordInput.validity.patternMismatch) {
-      if (!/[0-9]/.test(password)) {
-        validityMessage += "At least one digit ([0-9]).\n";
+  inputs.forEach((input) => {
+    let errorDiv = input.parentElement.querySelector(".requiredErrorMsg");
+    if (!input.value) {
+      e.preventDefault();
+      if (errorDiv) {
+        errorDiv.classList.add("showRequiredErrorMsg");
       }
-      if (!/[a-z]/.test(password)) {
-        validityMessage += "At least one lowercase letter ([a-z]).\n";
-      }
-      if (!/[A-Z]/.test(password)) {
-        validityMessage += "At least one uppercase letter ([A-Z]).\n";
-      }
-      if (password.length < 8) {
-        validityMessage += "A minimum length of 8 characters.\n";
-      }
-      passwordInput.setCustomValidity(validityMessage);
-      passwordInput.reportValidity();
     } else {
-      passwordInput.setCustomValidity("");
-    }
-  });
-
-  confirmPasswordInput.addEventListener("input", function () {
-    let password = passwordInput.value;
-    let confirmPassword = confirmPasswordInput.value;
-
-    if (password !== confirmPassword) {
-      confirmPasswordInput.setCustomValidity("Passwords do not match.");
-      confirmPasswordInput.reportValidity();
-    } else {
-      confirmPasswordInput.setCustomValidity("");
+      if (errorDiv) {
+        errorDiv.classList.remove("showRequiredErrorMsg");
+      }
     }
   });
 });
+
+emailInput.addEventListener("input", () => {
+  if (emailInput.validity.typeMismatch) {
+    emailInput.setCustomValidity("Please enter a valid email address.");
+    emailInput.reportValidity();
+  } else {
+    emailInput.setCustomValidity("");
+  }
+});
+
+countryInput.addEventListener("input", function () {
+  if (countryInput.validity.patternMismatch) {
+    countryInput.setCustomValidity("Please eneter a valid country name.");
+    countryInput.reportValidity();
+  } else {
+    countryInput.setCustomValidity("");
+  }
+});
+
+zipcodeInput.addEventListener("input", function () {
+  if (zipcodeInput.validity.patternMismatch) {
+    zipcodeInput.setCustomValidity("Zipcode must be 5 digits long");
+    zipcodeInput.reportValidity();
+  } else {
+    zipcodeInput.setCustomValidity("");
+  }
+});
+
+passwordInput.addEventListener("input", function () {
+  let password = passwordInput.value;
+  let validityMessage = "";
+
+  if (passwordInput.validity.patternMismatch) {
+    if (!/[0-9]/.test(password)) {
+      validityMessage += "At least one digit ([0-9]).\n";
+    }
+    if (!/[a-z]/.test(password)) {
+      validityMessage += "At least one lowercase letter ([a-z]).\n";
+    }
+    if (!/[A-Z]/.test(password)) {
+      validityMessage += "At least one uppercase letter ([A-Z]).\n";
+    }
+    if (password.length < 8) {
+      validityMessage += "A minimum length of 8 characters.\n";
+    }
+    passwordInput.setCustomValidity(validityMessage);
+    passwordInput.reportValidity();
+  } else {
+    passwordInput.setCustomValidity("");
+  }
+});
+
+confirmPasswordInput.addEventListener("input", function () {
+  let password = passwordInput.value;
+  let confirmPassword = confirmPasswordInput.value;
+
+  if (password !== confirmPassword) {
+    confirmPasswordInput.setCustomValidity("Passwords do not match.");
+    confirmPasswordInput.reportValidity();
+  } else {
+    confirmPasswordInput.setCustomValidity("");
+  }
+});
+
+console.log(countryInput.value);
