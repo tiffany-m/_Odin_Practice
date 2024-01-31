0da0eb14dfd4e4095850404efe3a3b76e038cf40
Tiffany <67019104+tiffany-m@users.noreply.github.co>
2024-01-31 17:53:57 -0500

    Added functions to api exercise

--- API Practice/scripts.js
+++ API Practice/scripts.js
@@ -7,31 +7,8 @@ const btn = document.getElementById("btn");
 const submit = document.getElementById("submit");
 const searchBar = document.getElementById("searchBar");
 
-function searchImage() {
-  fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=020EP8tODb1ySZNcVO7iWgWhEUqawzgT&s=dogs",
-    { mode: "cors" }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      img.src = response.data.images.original.url;
-      console.log(img.src);
-    });
-}
-
-function changeImage() {
-  fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=020EP8tODb1ySZNcVO7iWgWhEUqawzgT&s=dogs",
-    { mode: "cors" }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      img.src = response.data.images.original.url;
-    });
+function errorMessage() {
+  alert('Please enter a valid search term');
 }
 
 submit.addEventListener("click", () => {
@@ -46,23 +23,6 @@ submit.addEventListener("click", () => {
     })
     .then(function (response) {
       img.src = response.data.images.original.url;
-    });
+    })
+    .catch(errorMessage)
 });
-
-btn.addEventListener("click", () => {
-  changeImage();
-});
-
-// function changeImage() {
-//     fetch(
-//     "https://api.giphy.com/v1/gifs/translate?api_key=020EP8tODb1ySZNcVO7iWgWhEUqawzgT&s=dogs",
-//     { mode: "cors" }
-//   )
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (response) {
-//       img.src = response.data.images.original.url;
-//       console.log(img.src)
-//     });
-// }
