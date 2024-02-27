8e16cfd565a82542d15adc567b396d19d7855e2a
Tiffany <67019104+tiffany-m@users.noreply.github.co>
2024-02-27 11:56:59 -0500

    Started making Linked list

--- Linked List Project/scripts.js
+++ Linked List Project/scripts.js
@@ -1,3 +1,6 @@
+// https://www.youtube.com/watch?v=cob_kM0PJMo good video to watch for how to do basic linked list
+// list = [*Head Node* data: A, next: node] -> [data: B, next: node] -> [data: C, next: null] -> null
+
 //   - LinkedList class / factory, which will represent the full list.
 //   - Node class / factory, containing a value property and a link to the nextNode, set both as null by default.
 
@@ -13,3 +16,49 @@
 //  - contains(value) returns true if the passed in value is in the list and otherwise returns false.
 //  - find(value) returns the index of the node containing value, or null if not found.
 //  - toString represents your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> null
+
+class Node {
+  constructor(data) {
+    this.data = data, 
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null, 
+    this.length = 0;
+  }
+  append(data) {
+    const node = new Node(data);
+
+    // if list is empty, make first node the head
+    if (this.head === null) this.head = node;
+    else {
+      let current = this.head;
+      // keep iterating until you find the last node in list, add new node end of list
+      while (current.next != null) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.length++;
+  }
+
+  contains(value) {
+    let current = this.head;
+    while (current != null) {
+      if (current.data === value) return true;
+      current = current.next;
+    }
+    return false;
+  }
+}
+
+const list = new LinkedList();
+list.append("A");
+list.append("B");
+list.append("C");
+console.log(list);
+console.log(list.length);
+console.log(list.contains("A"));
