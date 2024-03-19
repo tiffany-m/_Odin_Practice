caf7567a6e6fad7308f82d47d8aff43e4b794f70
Tiffany <67019104+tiffany-m@users.noreply.github.co>
2024-03-19 14:33:43 -0400

    Practice

--- Graphs Practice/scripts.js
+++ Graphs Practice/scripts.js
@@ -1,8 +1,9 @@
 //  https://www.youtube.com/watch?v=bLtm94mvfjE
 // graph:  node/vertex, conected by edge/line
-// undirected: good for social network, could have user/node, if user is friends with another used that could be an edge
+// undirected: good for social network, could have user/node, if user is friends with another used that could be an edge, conntections go both ways
 // directed: edges link one node to another but they are one way connections, edges can link both ways, example would be map,
 //    street could be one way or two way
+//  NOTE: path between two vertices with the fewest edges a shortest path, path goes from a particular vertex back to itself, that's a cycle
 
 // adjacency matric: is a 2D array of size V x V where V is num of vertices/nodes in graph, each row and col represent a vertex/node, if the value of
 //    any element say, matrix[i][j], it represents that there is an edge connecting vertex i and j
@@ -115,3 +116,90 @@ graph.display(); // A ->, B -> C, C -> B
 
 graph.removeVertex("B");
 graph.display(); // A ->, C ->
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------
+/* A Queue object for queue-like functionality over JavaScript arrays. */
+let Queue = function() {
+    this.items = [];
+};
+Queue.prototype.enqueue = function(obj) {
+    this.items.push(obj);
+};
+Queue.prototype.dequeue = function() {
+    return this.items.shift();
+};
+Queue.prototype.isEmpty = function() {
+    return this.items.length === 0;
+};
+
+/*
+ * Performs a breadth-first search on a graph
+ * @param {array} graph - Graph, represented as adjacency lists.
+ * @param {number} source - The index of the source vertex.
+ * @returns {array} Array of objects describing each vertex, like
+ *     [{distance: _, predecessor: _ }]
+ */
+let doBFS = function(graph, source) {
+    let bfsInfo = [];
+
+    for (let i = 0; i < graph.length; i++) {
+	    bfsInfo[i] = {
+	        distance: null,
+	        predecessor: null };
+    }
+
+    bfsInfo[source].distance = 0;
+
+    let queue = new Queue();
+    queue.enqueue(source);
+
+// Traverse the graph
+
+    // As long as the queue is not empty:
+    //  Repeatedly dequeue a vertex u from the queue.
+    //  
+    //  For each neighbor v of u that has not been visited:
+    //     Set distance to 1 greater than u's distance
+    //     Set predecessor to u
+    //     Enqueue v
+    //
+    //  Hint:
+    //  use graph to get the neighbors,
+    //  use bfsInfo for distances and predecessors 
+        while (!queue.isEmpty()) {
+          // pop queue item into vertex variable
+          var vertex = queue.dequeue();
+
+          // iterate through vertex subarray in graph
+          for (var u = 0; u < graph[vertex].length; u++) {
+            // set neighbor var to be the current index iteration
+            var neighbor = graph[vertex][u];
+
+            // check if neighbor has been visited
+            if (bfsInfo[neighbor].distance === null) {
+              // add 1 to the distance of current vertex, then set for neighbor
+              bfsInfo[neighbor].distance = bfsInfo[vertex].distance + 1;
+              // origin for the current vertex
+              bfsInfo[neighbor].predecessor = vertex;
+              queue.enqueue(neighbor);
+            }
+          }
+        }
+
+    return bfsInfo;
+};
+
+let adjList = [
+    [1],
+    [0, 4, 5],
+    [3, 4, 5],
+    [2, 6],
+    [1, 2],
+    [1, 2, 6],
+    [3, 5],
+    []
+    ];
+let bfsInfo = doBFS(adjList, 3);
+for (let i = 0; i < adjList.length; i++) {
+    console.log("vertex " + i + ": distance = " + bfsInfo[i].distance + ", predecessor = " + bfsInfo[i].predecessor);
+}
